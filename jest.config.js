0db7b079cd6c62355dac84c30fc6012ebceef3f5
pedrooliver13 <pedro@qyon.com>
2021-03-12 13:01:45 -0300

    add first style e test style

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', '/.next/'],
   collectCoverage: true,
-  collectConverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
-}
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/'],
+  moduleNameMapper: {
+    'styled-components':
+      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js',
+  },
+};
